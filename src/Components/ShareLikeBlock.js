90ee3245b85ed7385402e69338513002976fdd93
ymalakhov <it.malakhov@gmail.com>
2022-06-04 15:08:17 +0200

    add alert on sharebtn click

--- src/Components/ShareLikeBlock.js
+++ src/Components/ShareLikeBlock.js
@@ -1,17 +1,56 @@
 import React from "react";
+import { useState } from "react";
 import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
-import { Container, IconButton } from "@mui/material";
+import { Container, IconButton, Button } from "@mui/material";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
 export default function ShareLikeBlock() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div>
-      <IconButton area-lable="share button">
-        <ShareOutlinedIcon color="primary" sx={{ mr: 1 }} />
-      </IconButton>
-      <IconButton area-lable="like button">
-        <ThumbUpOutlinedIcon color="primary" />
-      </IconButton>
-    </div>
+    <>
+      <div>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            {"Share this article with your friends!"}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Here should be social media icons with href to websides.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>close</Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+
+      <div>
+        <IconButton area-lable="share button" onClick={handleClickOpen}>
+          <ShareOutlinedIcon color="primary" sx={{ mr: 1 }} />
+        </IconButton>
+        <IconButton area-lable="like button">
+          <ThumbUpOutlinedIcon color="primary" />
+        </IconButton>
+      </div>
+    </>
   );
 }

ee4d6abbe0f9c83cbc54efa4c5c6f826599e248b
predavladc <predavladc@gmail.com>
2022-06-11 16:28:24 +0200

    Blog homepage styling

--- src/Components/BlogPreview.js
+++ src/Components/BlogPreview.js
@@ -1,11 +1,11 @@
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import { Link } from "react-router-dom";
-import Typography from "@mui/material/Typography";
+import { Typography, Container, Box, Grid } from "@mui/material";
 import { Button } from "@mui/material";
 // import { parseISO, formatDistance } from "date-fns";
 import DatePosted from "./DatePosted";
 
-const BlogPreview = ({ title, author, summary, postDate }) => {
+const BlogPreview = ({ title, author, summary, postDate, heroImage }) => {
   // console.log(postDate);
 
   // const formatDate = (postDate) => {
@@ -18,23 +18,38 @@ const BlogPreview = ({ title, author, summary, postDate }) => {
   //   return diff;
   // };
 
+  const imageSeen = `http:${heroImage.fields.file.url}`;
   return (
     <div>
       <Typography variant="h4">{title}</Typography>
-      <Button
-        component={Link}
-        to={`/author/${author.sys.id}`}
-        variant="text"
-        color="secondary"
-      >
-        <Typography variant="overline">{author.fields.name}, </Typography>
-      </Button>      
-      <span>posted  </span>
-      {/* {formatDate(postDate)}  */}
-      <DatePosted date={postDate} />
-      <Typography variant="body1"> 
-        {documentToReactComponents(summary)}
-      </Typography>
+      <Grid container spacing={2}>
+        
+        <Grid item xs={9}>
+          <Button
+            component={Link}
+            to={`/author/${author.sys.id}`}
+            variant="text"
+            color="secondary"
+          >
+            <Typography variant="overline">{author.fields.name}, </Typography>
+          </Button>
+          <span>posted </span>
+          {/* {formatDate(postDate)}  */}
+          <DatePosted date={postDate} />
+
+          <Typography variant="body1">
+            {documentToReactComponents(summary)}
+          </Typography>
+        </Grid>
+        <Grid item xs={3} align='center'>
+          <Box
+            component="img"
+            className="RockLogo blogImgPreview"
+            alt="Logo"
+            src={imageSeen}
+          />
+        </Grid>
+      </Grid>
     </div>
   );
 };

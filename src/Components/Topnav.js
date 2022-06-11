2cc1de23011b9c7e2ba2a978b9eb749f8c870bd0
ymalakhov <it.malakhov@gmail.com>
2022-06-11 09:31:25 +0200

    ready to remove sidenav

--- src/Components/Topnav.js
+++ src/Components/Topnav.js
@@ -8,11 +8,13 @@ import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
+import SearchIcon from "@mui/icons-material/Search";
+import InputBase from "@mui/material/InputBase";
+import { styled, alpha } from "@mui/material/styles";
+import rockicon from "../media/rock.svg";
+import { NavLink } from "react-router-dom";
 
 const pages = [
   "All News",
@@ -22,6 +24,48 @@ const pages = [
   "Rock",
   "Electronic",
 ];
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
 
 export default function Topnav() {
   const [anchorElNav, setAnchorElNav] = useState(null);
@@ -44,17 +88,34 @@ export default function Topnav() {
 
   return (
     <nav>
-      <AppBar position="static">
+      <AppBar position="static" color="transparent">
         <Container maxWidth="xl">
           <Toolbar disableGutters>
-            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <Box
+              component="img"
+              className="RockLogo"
+              alt="Logo"
+              src={rockicon}
+              sx={{
+                width: 50,
+                height: "auto",
+                filter: "invert(1)",
+                display: { xs: "none", md: "flex" },
+              }}
+            />
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: { xs: "flex", md: "none" },
+              }}
+            >
               <IconButton
                 size="large"
                 aria-label="account of current user"
                 aria-controls="menu-appbar"
                 aria-haspopup="true"
                 onClick={handleOpenNavMenu}
-                color="inherit"
+                sx={{ color: "white" }}
               >
                 <MenuIcon />
               </IconButton>
@@ -78,11 +139,27 @@ export default function Topnav() {
               >
                 {pages.map((page) => (
                   <MenuItem key={page} onClick={handleCloseNavMenu}>
-                    <Typography textAlign="center">{page}</Typography>
+                    <NavLink to=''>
+                      <Typography textAlign="center">{page}</Typography>
+                    </NavLink>
                   </MenuItem>
                 ))}
               </Menu>
             </Box>
+            <Box
+              component="img"
+              className="RockLogo"
+              alt="Logo"
+              src={rockicon}
+              ml={7}
+              mr={7}
+              sx={{
+                width: 50,
+                height: "auto",
+                filter: "invert(1)",
+                display: { xs: "flex", md: "none" },
+              }}
+            />
             <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
               {pages.map((page) => (
                 <Button
@@ -94,6 +171,15 @@ export default function Topnav() {
                 </Button>
               ))}
             </Box>
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search…"
+                inputProps={{ "aria-label": "search" }}
+              />
+            </Search>
           </Toolbar>
         </Container>
       </AppBar>
